7fc3ff13b4910dfc0c04d2772918aadafe409223
sachinaloysius <sachinaloysius07@gmail.com>
2023-04-05 12:14:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import StateTutorial from './UseState/StateTutorial'
+import RefTutorial from './UseRef/RefTutorial'
+import ReducerTutorial from './UseReducer/ReducerTutorial'
+import MemoTutorial from './UseMemo/MemoTutorial'
+import LayoutEffectTutorial from './UseLayoutEffect/LayoutEffectTutorial'
+import ImperativeHandle from './UseImperativeHandle/ImperativeHandle'
+import EffectTutorial from './UseEffect/EffectTutorial'
+import ContextTutorial from './UseContext/ContextTutorial'
+import CallBackTutorial from './UseCallback/CallbackTutorial'
+
+export default function App() {
+  return (
+    <StateTutorial/>
+    // <RefTutorial/>
+    // <ReducerTutorial/>
+    // <EffectTutorial/>
+    // <MemoTutorial/>
+    // <LayoutEffectTutorial/>
+    // <ImperativeHandle/>
+    // <ContextTutorial/>
+    // <CallBackTutorial/>
+  )
+}
